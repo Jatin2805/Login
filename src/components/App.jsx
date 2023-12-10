8379e3cc533a3177320e11118ee31ff90aa319a3
Jatin2805 <jatin.seelam@gmail.com>
2023-12-10 19:38:41 +0530

    Login

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import Login from './Login'
+
+import Register from './Register';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+function App() {
+
+  return (
+    
+    <>
+    
+    <BrowserRouter >
+    <Routes>
+  
+   
+    <Route path="/Login" element ={<Login/>} />
+    
++    
++    <Route path="/Register" element ={<Register/>} />
+
+    </Routes>
+    </BrowserRouter>
+    
+    
+    </>
+
+    
+  );
+}
+
+export default App;
