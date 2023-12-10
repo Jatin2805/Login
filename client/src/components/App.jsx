013b21758ce244be4292d674e4bb50ea361d4691
Revanth Sallangula <revanth.sallangula@icloud.com>
2023-12-10 20:14:54 +0530

    cleanup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,32 +1,19 @@
-import Login from './Login'
+import Login from "./Login";
 
-import Register from './Register';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import Register from "./Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    
-    <>
-    
-    <BrowserRouter >
-    <Routes>
-  
-   
-    <Route path="/Login" element ={<Login/>} />
-    
-+    
-+    <Route path="/Register" element ={<Register/>} />
-
-    </Routes>
-    </BrowserRouter>
-    
-    
-    </>
-
-    
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/Login" element={<Login />} />
+                    + + <Route path="/Register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,51 +1,67 @@
 // import '../styles/First.css'
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 // import Form from 'react-bootstrap/Form';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
-import React from 'react'
-import 'boxicons'
-import '../styles/Login.css'
+import React from "react";
+import "boxicons";
+import "../styles/Login.css";
 // import './aac.css'
 const Login = () => {
-    
-    return(
-        
-   
-  <div >
-  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="Login.css" />
-  {/* <link rel="stylesheet" href="aac.css" /> */}
-  <div>
-    <div className="wrapper">
-      <form action="">
-        <h1>Login</h1>
-        <div className="input-box">
-          <input type="text" placeholder="Username" required />
-         <i class='bx bx-user' ></i>
+    return (
+        <div>
+            <link
+                href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+                rel="stylesheet"
+            />
+            <link rel="stylesheet" href="Login.css" />
+            {/* <link rel="stylesheet" href="aac.css" /> */}
+            <div>
+                <div className="wrapper">
+                    <form action="">
+                        <h1>Login</h1>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                required
+                            />
+                            <i class="bx bx-user"></i>
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                required
+                            />
+                            <i class="bx bxs-lock-alt"></i>
+                        </div>
+                        <div className="remember-forgot">
+                            <label>
+                                <input type="checkbox" />
+                                Remember
+                            </label>
+                            <a href="#">Forgot Password</a>
+                        </div>
+                        <div>
+                            <Button
+                                type="button"
+                                className="btn"
+                                style={{ marginLeft: 40, width: "40" }}
+                            >
+                                Login
+                            </Button>
+                        </div>
+                        <div className="register-link">
+                            <p>
+                                dont have an account
+                                <a href="/Register"> Register</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="input-box">
-          <input type="password" placeholder="Password" required />
-          <i class='bx bxs-lock-alt'></i>
-        </div>
-        <div className="remember-forgot">
-          <label><input type="checkbox" />Remember</label>
-          <a href="#">Forgot Password</a>
-        </div>
-        <div >
-        <Button type="button" className="btn" style={{marginLeft:40,width:"40"}}>Login</Button>
-        </div>
-        <div className="register-link">
-          <p>dont have an account
-            <a href='/Register'> Register</a></p>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-
-  )
-}
-export default Login
-    
+    );
+};
+export default Login;
